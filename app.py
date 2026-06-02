c39955c3be5c45b6ffc7681db8aa7d6a99929e22
пименов алексей <aleshapimenov2006@gmail.com>
2026-06-02 04:21:08 +0300

    Fix: add gunicorn and simplify app

--- app.py
+++ app.py
@@ -68,4 +68,6 @@ with app.app_context():
     print('База данных готова к работе!')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
